9460a37b1d38e6c88d3eb708097fcf03f2da9bf9
cptHarlan <133979321+cptHarlan@users.noreply.github.com>
2025-11-20 08:22:33 +0100

    Update settings.js

--- nodered/settings.js
+++ nodered/settings.js
@@ -1,1 +1,22 @@
-
+module.exports = {
+    uiPort: process.env.NODERED_HTTP_PORT || 1880,
+    adminAuth: {
+        type: "credentials",
+        users: [{
+            username: "admin",
+            password: "$2b$08$4o/6HNUkaRO3COMD6QjKOONt1mKx5RwWFA7pN7a7JMBF73qOJr9qG", //Stegra18
+            permissions: "*"
+        }]
+    },
+    telemetry: {
+        enabled: false
+    },
+    logging: {
+        console: {
+            level: "info",
+            metrics: false,
+            audit: false
+        }
+    },
+    userDir: "/data"
+};
